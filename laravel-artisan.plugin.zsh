037c26acb4f21e93adbffb03486ce858f62bfd28
crazybooot <crazybooot@gmail.com>
2016-12-14 15:44:57 +0200

    add zsh plugin file

---
+++ laravel-artisan.plugin.zsh
@@ -0,0 +1,83 @@
+# laravel 5.3 artisan command aliases
+
+alias a='php artisan'
+alias acc='php artisan clear-compiled'
+alias ad='php artisan down'
+alias ae='php artisan env'
+alias ah='php artisan help'
+alias ai='php artisan inspire'
+alias al='php artisan list'
+alias ao='php artisan optimize'
+alias as='php artisan serve'
+alias at='php artisan tinker'
+alias au='php artisan up'
+alias aanm='php artisan app:name'
+alias aacr='php artisan auth:clear-resets'
+# cache
+alias accl='php artisan cache:clear'
+alias actb='php artisan cache:table'
+# config
+alias acfcc='php artisan config:cache'
+alias acfcl='php artisan config:clear'
+#
+alias adbs='php artisan db:seed'
+alias aeg='php artisan event:generate'
+alias akg='php artisan key:generate'
+# make
+alias amkau='php artisan make:auth'
+alias amkcm='php artisan make:command'
+alias amkct='php artisan make:controller'
+alias amkev='php artisan make:event'
+alias amkjb='php artisan make:job'
+alias amkls='php artisan make:listener'
+alias amkml='php artisan make:mail'
+alias amkmw='php artisan make:middleware'
+alias amkmg='php artisan make:migration'
+alias amkmd='php artisan make:model'
+alias amknf='php artisan make:notification'
+alias amkpl='php artisan make:policy'
+alias amkpv='php artisan make:provider'
+alias amkrq='php artisan make:request'
+alias amksd='php artisan make:seeder'
+alias amkts='php artisan make:test'
+# migrate
+alias amg='php artisan migrate'
+alias amgis='php artisan migrate:install'
+alias amgrf='php artisan migrate:refresh'
+alias amgrs='php artisan migrate:reset'
+alias amgrb='php artisan migrate:rollback'
+alias amgst='php artisan migrate:status'
+# queue
+alias aqf='php artisan queue:failed'
+alias aqft='php artisan queue:failed-table'
+alias aqfl='php artisan queue:flush'
+alias aqfg='php artisan queue:forget'
+alias aqls='php artisan queue:listen'
+alias aqrs='php artisan queue:restart'
+alias aqrt='php artisan queue:retry'
+alias aqtb='php artisan queue:table'
+alias aqwk='php artisan queue:work'
+# route
+alias arcc='php artisan route:cache'
+alias arcl='php artisan route:clear'
+alias arls='php artisan route:list'
+#
+alias asrn='php artisan schedule:run'
+alias astb='php artisan session:table'
+alias asln='php artisan storage:link'
+alias avpb='php artisan vendor:publish'
+alias avcl='php artisan view:clear'
+
+# laravel 5.2 artisan command aliases
+
+alias amkcs='php artisan make:console'
+
+# laravel 5.1 artisan command aliases
+
+alias ahcm='php artisan handler:command'
+alias ahev='php artisan handler:event'
+alias aqss='php artisan queue:subscribe'
+
+# laravel 5.0 artisan command aliases
+
+alias afr='php artisan fresh'
